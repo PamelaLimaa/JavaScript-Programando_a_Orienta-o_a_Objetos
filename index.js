e5e09c7129bbd3e34c331b5ee9525f4418f507e7
PamelaLimaa <layanepamela98@gmail.com>
2022-08-04 20:41:57 -0300

    index.js

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
-    rg;
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente{
+    agencia;
+    // #saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
 
+    sacar(valor){
+        if (this._saldo >= valor) {
+            this._saldo -= valor
+
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if (valor <= 0) {
+            return;
+        }
+        this._saldo += valor
+    }
+}
+
+
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf= 11112234;
-cliente1.agencia = 1101;
-cliente1.saldo = 3;
-cliente1.rg = 2676165167;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice"
 cliente2.cpf = 11112234;
-cliente2.agencia = 1101;
-cliente2.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+const valorSacado = contaCorrenteRicardo.sacar(50);
 
-console.log(cliente1, cliente2);
+console.log(cliente1);
+console.log(cliente2);
