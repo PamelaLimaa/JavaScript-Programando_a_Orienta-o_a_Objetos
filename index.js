dcfabfce40ea2c480b161c14e3d653f113d89cea
PamelaLimaa <layanepamela98@gmail.com>
2022-08-04 23:18:52 -0300

    index.js

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ cliente2.nome = "Alice"
 cliente2.cpf = 11112234;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
-contaCorrenteRicardo.agencia = 1001;
-const valorSacado = contaCorrenteRicardo.sacar(50);
+
 
 console.log(cliente1);
 console.log(cliente2);
